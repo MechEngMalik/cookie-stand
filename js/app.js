e5e98e3b06bcf227fa117fd532868699c775bd09
MechEngMalik <mecheng.malik93@gmail.com>
2021-06-28 03:42:51 +0300

    fixing some errors

--- js/app.js
+++ js/app.js
@@ -9,9 +9,12 @@ let seattle = {
   maxCust:65 ,
   avgCookies:6.3,
   cfh:0,
+  // costumer For Hour
   hSales:[],
-  hour : ['6 am: ','7 am: ','8 am: ','9 am: ','10am: ','11 am: ','12 pm: ','1 pm: ','2 pm: ','3 pm: ','4 pm: ','5 pm: ','6pm:  ','7 pm: ','Total: '],
+  // hourly sales
+  hour : ['6 am: ','7 am: ','8 am: ','9 am: ','10am: ','11 am: ','12 pm: ','1 pm: ','2 pm: ','3 pm: ','4 pm: ','5 pm: ','6pm:  ','7 pm: '],
 
+  // costumer per hour
   cph : function () {
 
     return Math.floor(Math.random()*(this.maxCust-this.minCust + 1) + this.minCust);
@@ -36,43 +39,39 @@ let seattle = {
   render: function(){
     this.hourlyTotal();
 
-    let divE1  = document.getElementById('stores sale');
-    let articlE1 = document.createElement('article');
-    divE1.appendChild(articlE1);
-
-
-    let h2E1 = document.createElement('h2');
-    articlE1.appendChild(h2E1);
-    h2E1.textContent = this.name;
+    // let divE1  = document.getElementById('stores sale');
 
 
 
+    // let h2E1 = document.createElement('h2');
+    // divE1.appendChild(h2E1);
+    // h2E1.textContent = this.name;
 
+
+
+    let divul = document.getElementById('list');
     let ulE1 = document.createElement('ul');
+    let h2l1 = document.createElement('h2');
+    h2l1.textContent = (this.name);
+    divul.appendChild(h2l1);
+    divul.appendChild(ulE1);
 
     for (let i = 0; i < this.hour.length; i++) {
 
       let liE1 = document.createElement('li');
-      liE1.textContent = this.hour[i];
-
-      console.log(liE1.textContent = this.hour[i] , this.cph);
+      liE1.textContent = this.hour[i]+this.cph(23,65);
 
       ulE1.appendChild(liE1);
 
 
 
     }
-    let nE1 = document.createElement('ul');
-    for (let i = 0; i < this.hour.length; i++) {
-      let n1E1 = document.createElement('sales');
-      n1E1.textContent= this.cph[i];
-      nE1.appendChild(n1E1);
 
-
-      articlE1.appendChild(nE1);
-
-      articlE1.appendChild(ulE1);
-    }
+    let total = this.cfh;
+    console.log(total);
+    let tCookies = document.createElement('li');
+    divul.appendChild(tCookies);
+    tCookies.textContent = ('Toatal' + this.cfh);
 
 
 
@@ -81,3 +80,360 @@ let seattle = {
 };
 
 seattle.render();
+
+
+
+
+
+
+
+let tokyo = {
+  name: 'tokyo',
+  minCust:23 ,
+  maxCust:65 ,
+  avgCookies:6.3,
+  cfh:0,
+  // costumer For Hour
+  hSales:[],
+  // hourly sales
+  hour : ['6 am: ','7 am: ','8 am: ','9 am: ','10am: ','11 am: ','12 pm: ','1 pm: ','2 pm: ','3 pm: ','4 pm: ','5 pm: ','6pm:  ','7 pm: '],
+
+  // costumer per hour
+  cph : function () {
+
+    return Math.floor(Math.random()*(this.maxCust-this.minCust + 1) + this.minCust);
+
+
+  },
+  calcPerHr :function(){
+    return Math.floor(this.calcRandHrlyCust()*this.avgCookies);
+
+  },
+
+  hourlyTotal(){
+    for (let i = 0; i <14; i++) {
+      this.hSales[i] = this.cph();
+      this.cfh += this.hSales[i];
+    }
+  },
+
+
+
+
+  render: function(){
+    this.hourlyTotal();
+
+    // let divE1  = document.getElementById('stores sale');
+
+
+
+    // let h2E1 = document.createElement('h2');
+    // divE1.appendChild(h2E1);
+    // h2E1.textContent = this.name;
+
+
+
+    let divul = document.getElementById('list');
+    let ulE1 = document.createElement('ul');
+    let h2l1 = document.createElement('h2');
+    h2l1.textContent = (this.name);
+    divul.appendChild(h2l1);
+    divul.appendChild(ulE1);
+
+    for (let i = 0; i < this.hour.length; i++) {
+
+      let liE1 = document.createElement('li');
+      liE1.textContent = this.hour[i]+this.cph(23,65);
+
+      ulE1.appendChild(liE1);
+
+
+
+    }
+
+    let total = this.cfh;
+    console.log(total);
+    let tCookies = document.createElement('li');
+    divul.appendChild(tCookies);
+    tCookies.textContent = ('Toatal' + this.cfh);
+
+
+
+  }
+
+};
+
+tokyo.render();
+
+
+
+
+
+let dubai = {
+  name: 'dubai',
+  minCust:23 ,
+  maxCust:65 ,
+  avgCookies:6.3,
+  cfh:0,
+  // costumer For Hour
+  hSales:[],
+  // hourly sales
+  hour : ['6 am: ','7 am: ','8 am: ','9 am: ','10am: ','11 am: ','12 pm: ','1 pm: ','2 pm: ','3 pm: ','4 pm: ','5 pm: ','6pm:  ','7 pm: '],
+
+  // costumer per hour
+  cph : function () {
+
+    return Math.floor(Math.random()*(this.maxCust-this.minCust + 1) + this.minCust);
+
+
+  },
+  calcPerHr :function(){
+    return Math.floor(this.calcRandHrlyCust()*this.avgCookies);
+
+  },
+
+  hourlyTotal(){
+    for (let i = 0; i <14; i++) {
+      this.hSales[i] = this.cph();
+      this.cfh += this.hSales[i];
+    }
+  },
+
+
+
+
+  render: function(){
+    this.hourlyTotal();
+
+    // let divE1  = document.getElementById('stores sale');
+
+
+
+    // let h2E1 = document.createElement('h2');
+    // divE1.appendChild(h2E1);
+    // h2E1.textContent = this.name;
+
+
+
+    let divul = document.getElementById('list');
+    let ulE1 = document.createElement('ul');
+    let h2l1 = document.createElement('h2');
+    h2l1.textContent = (this.name);
+    divul.appendChild(h2l1);
+    divul.appendChild(ulE1);
+
+    for (let i = 0; i < this.hour.length; i++) {
+
+      let liE1 = document.createElement('li');
+      liE1.textContent = this.hour[i]+this.cph(23,65);
+
+      ulE1.appendChild(liE1);
+
+
+
+    }
+
+    let total = this.cfh;
+    console.log(total);
+    let tCookies = document.createElement('li');
+    divul.appendChild(tCookies);
+    tCookies.textContent = ('Toatal' + this.cfh);
+
+
+
+  }
+
+};
+
+dubai.render();
+
+
+
+
+
+let paris = {
+  name: 'paris',
+  minCust:23 ,
+  maxCust:65 ,
+  avgCookies:6.3,
+  cfh:0,
+  // costumer For Hour
+  hSales:[],
+  // hourly sales
+  hour : ['6 am: ','7 am: ','8 am: ','9 am: ','10am: ','11 am: ','12 pm: ','1 pm: ','2 pm: ','3 pm: ','4 pm: ','5 pm: ','6pm:  ','7 pm: '],
+
+  // costumer per hour
+  cph : function () {
+
+    return Math.floor(Math.random()*(this.maxCust-this.minCust + 1) + this.minCust);
+
+
+  },
+  calcPerHr :function(){
+    return Math.floor(this.calcRandHrlyCust()*this.avgCookies);
+
+  },
+
+  hourlyTotal(){
+    for (let i = 0; i <14; i++) {
+      this.hSales[i] = this.cph();
+      this.cfh += this.hSales[i];
+    }
+  },
+
+
+
+
+  render: function(){
+    this.hourlyTotal();
+
+    // let divE1  = document.getElementById('stores sale');
+
+
+
+    // let h2E1 = document.createElement('h2');
+    // divE1.appendChild(h2E1);
+    // h2E1.textContent = this.name;
+
+
+
+    let divul = document.getElementById('list');
+    let ulE1 = document.createElement('ul');
+    let h2l1 = document.createElement('h2');
+    h2l1.textContent = (this.name);
+    divul.appendChild(h2l1);
+    divul.appendChild(ulE1);
+
+    for (let i = 0; i < this.hour.length; i++) {
+
+      let liE1 = document.createElement('li');
+      liE1.textContent = this.hour[i]+this.cph(23,65);
+
+      ulE1.appendChild(liE1);
+
+
+
+    }
+
+    let total = this.cfh;
+    console.log(total);
+    let tCookies = document.createElement('li');
+    divul.appendChild(tCookies);
+    tCookies.textContent = ('Toatal' + this.cfh);
+
+
+
+  }
+
+};
+
+paris.render();
+
+
+
+
+
+
+let lima = {
+  name: 'lima',
+  minCust:23 ,
+  maxCust:65 ,
+  avgCookies:6.3,
+  cfh:0,
+  // costumer For Hour
+  hSales:[],
+  // hourly sales
+  hour : ['6 am: ','7 am: ','8 am: ','9 am: ','10am: ','11 am: ','12 pm: ','1 pm: ','2 pm: ','3 pm: ','4 pm: ','5 pm: ','6pm:  ','7 pm: '],
+
+  // costumer per hour
+  cph : function () {
+
+    return Math.floor(Math.random()*(this.maxCust-this.minCust + 1) + this.minCust);
+
+
+  },
+  calcPerHr :function(){
+    return Math.floor(this.calcRandHrlyCust()*this.avgCookies);
+
+  },
+
+  hourlyTotal(){
+    for (let i = 0; i <14; i++) {
+      this.hSales[i] = this.cph();
+      this.cfh += this.hSales[i];
+    }
+  },
+
+
+
+
+  render: function(){
+    this.hourlyTotal();
+
+    // let divE1  = document.getElementById('stores sale');
+
+
+
+    // let h2E1 = document.createElement('h2');
+    // divE1.appendChild(h2E1);
+    // h2E1.textContent = this.name;
+
+
+
+    let divul = document.getElementById('list');
+    let ulE1 = document.createElement('ul');
+    let h2l1 = document.createElement('h2');
+    h2l1.textContent = (this.name);
+    divul.appendChild(h2l1);
+    divul.appendChild(ulE1);
+
+    for (let i = 0; i < this.hour.length; i++) {
+
+      let liE1 = document.createElement('li');
+      liE1.textContent = this.hour[i]+this.cph(23,65);
+
+      ulE1.appendChild(liE1);
+
+
+
+    }
+
+    let total = this.cfh;
+    console.log(total);
+    let tCookies = document.createElement('li');
+    divul.appendChild(tCookies);
+    tCookies.textContent = ('Toatal' + this.cfh);
+
+
+
+  }
+
+};
+
+lima.render();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
